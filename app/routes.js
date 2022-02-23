a1c4b8df375d71bdac790fa6b7b9a6b4a9ea6a00
mattlavis-transform <matthew.lavis@gmail.com>
2022-02-23 18:50:04 +0000

    Access Heroku API

--- app/routes.js
+++ app/routes.js
@@ -8,7 +8,8 @@ const Commodity = require('./classes/commodity.js');
 // Get a commodity
 router.get(['/commodities/:goods_nomenclature_item_id/'], function (req, res) {
     var goods_nomenclature_item_id = req.params["goods_nomenclature_item_id"];
-    var url = 'http://127.0.0.1:5000/commodities/' + goods_nomenclature_item_id;
+    // var url = 'http://127.0.0.1:5000/commodities/' + goods_nomenclature_item_id;
+    var url = 'https://comm-code-history.herokuapp.com/commodities/' + goods_nomenclature_item_id;
     axios.get(url)
         .then((response) => {
             var commodity = new Commodity(response)
@@ -17,6 +18,5 @@ router.get(['/commodities/:goods_nomenclature_item_id/'], function (req, res) {
             });
         });
 });
-
 
 module.exports = router
